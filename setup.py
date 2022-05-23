4b28c94abdfcb8b4e724ab9cb562ba31711339f6
Avra <96898985+avrabyt@users.noreply.github.com>
2022-05-23 23:28:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
-import setuptools
+from setuptools import setup
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="st_speckmol",
