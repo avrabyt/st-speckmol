eb0b0b77a67b8a7928b0b0ea1ef6d4ef02a7e419
AvratanuBiswas <a.biswas@vu.nl>
2022-05-23 15:45:00 +0200

    :heavy_plus_sign: added init file and tested #1

---
+++ test.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import glob
+from speckmol.funx import spec_plot
+
+# Example files path
+ex_files = glob.glob("examples/*.xyz")
+with st.sidebar:
+    example_xyz = st.selectbox("Select a molecule",ex_files)
+    f = open(example_xyz,"r")
+    example_xyz = f.read()
+
+res = spec_plot(example_xyz)
