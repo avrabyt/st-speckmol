ab074185f517d2ae43c27e9e7ff3ec1d409e3a1e
AvratanuBiswas <a.biswas@vu.nl>
2022-05-24 10:37:34 +0200

    :recycle: updated folder name to xyz_mol_examples #1 :sparkles:

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import glob
 from st_speckmol import spec_plot
 
 # Example files path
-ex_files = glob.glob("examples/*.xyz")
+ex_files = glob.glob("xyz_mol_examples/*.xyz")
 with st.sidebar:
     example_xyz = st.selectbox("Select a molecule",ex_files)
     f = open(example_xyz,"r")
