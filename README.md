6b39503cbf98c39df388b18e28914a0eff9ed48d
AvratanuBiswas <a.biswas@vu.nl>
2022-06-28 14:50:14 +0200

    :rocket: v0.0.5 :sparkles:

--- README.md
+++ README.md
@@ -42,7 +42,7 @@ res = spec_plot(example_xyz)
 ```
 [![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://share.streamlit.io/avrabyt/specklit/main/app.py)
 
-![Speclit demo](https://github.com/avrabyt/Specklit/blob/main/SpeckLit_demo.gif)
+![Speclit demo](https://github.com/avrabyt/Specklit/blob/main/Resources/SpeckLit_demo.gif)
 
 Meanwhile,[ipyspeck](https://pypi.org/project/ipyspeck/) in their latest release ` 0.6.1 ` has added the stspec module [https://github.com/avrabyt/Specklit/issues/1#issuecomment-1134798584], therefore feel free to use whatever convinient, as long as you are interested to have fun with beautiful speck strcutures 🧬 and streamlit 🎈 🎉
 
---
+++ pages/Example-1-Basic.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import glob
+from st_speckmol import spec_plot
+
+st.markdown('''# st-speckmol :package:
+_A Streamlit **Component** for creating Speck molecular structures within Streamlit Web app._
+''')
+
+# Example files path
+ex_files = glob.glob("xyz_mol_examples/*.xyz")
+with st.sidebar:
+    example_xyz = st.selectbox("Select a molecule",ex_files)
+    f = open(example_xyz,"r")
+    example_xyz = f.read()
+    st.sidebar.info(example_xyz.splitlines()[1])
+
+res = spec_plot(example_xyz)
+with st.sidebar.expander('Code', expanded=False):
+    st.code(
+        '''
+        import streamlit as st
+import glob
+from st_speckmol import spec_plot
+
+# Example files path
+ex_files = glob.glob("xyz_mol_examples/*.xyz")
+with st.sidebar:
+    example_xyz = st.selectbox("Select a molecule",ex_files)
+    f = open(example_xyz,"r")
+    example_xyz = f.read()
+    st.sidebar.info(example_xyz.splitlines()[1])
+
+res = spec_plot(example_xyz)
+        '''
+    )
