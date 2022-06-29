62a35c9d302a7633f4636fec08dd6cc1a5af4658
Avra <96898985+avrabyt@users.noreply.github.com>
2022-06-29 11:09:35 +0200

    Update Example-2-Parameters-Usage.py

--- pages/Example-2-Parameters-Usage.py
+++ pages/Example-2-Parameters-Usage.py
@@ -48,11 +48,11 @@ with st.expander('Code',expanded=False):
         bond = st.checkbox('Bond',value=True)
         bond_scale = st.slider('BondScale',min_value=0.0,max_value=1.0,value=0.8)
         brightness = st.slider('Brightness',min_value=0.0,max_value=1.0,value=0.4)
-        relativeAtomScale = st.slider('RelativeAtomScale',min_value=0.0,max_value=1.0,value=0.64)
-        bondShade = st.slider('bondShade',min_value=0.0,max_value=1.0,value=0.5)
+        relativeAtomScale = st.slider('Relative atom scale',min_value=0.0,max_value=1.0,value=0.64)
+        atomShade  = st.slider('Atom shade',min_value=0.0,max_value=1.0,value=0.5)
 
     _PARAMETERS = {'outline': outl , 'bondScale': bond_scale,
-                    'bonds': bond ,'bondShade' : bondShade,
+                    'bonds': bond ,'atomShade' : atomShade,
                     'brightness': brightness, 'relativeAtomScale':relativeAtomScale,
                     }
     res = spec_plot(example_xyz,_PARAMETERS = _PARAMETERS,wbox_height="500px",wbox_width="500px")
