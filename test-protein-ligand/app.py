8f7bea5b6a4fe6965a131318c896687456ac5049
avrabyt <avrab.yt@gmail.com>
2022-07-05 11:37:55 +0200

    :triangular_flag_on_post: updated with protein-ligand codes

--- test-protein-ligand/app.py
+++ test-protein-ligand/app.py
@@ -1,6 +1,6 @@
 import py3Dmol
 import streamlit as st
-
+from stmol import showmol
 
 def visbox2(objeto,bxi,byi,bzi,bxf,byf,bzf):  
     objeto.addBox({'center':{'x':bxi,'y':byi,'z':bzi},'dimensions': {'w':bxf,'h':byf,'d':bzf},'color':'blue','opacity': 0.5})
@@ -21,16 +21,18 @@ def vismol(bxi=-10,byi=-10,bzi=-10,bxf=5,byf=5,bzf=5):
     mol_view.rotate(90, {'x':0,'y':1,'z':0},viewer=(0,1));
     mol_view.zoomTo()  
     mol_view.show()
+    showmol(mol_view, height = 500,width=800)
 
-from ipywidgets import interact,fixed,IntSlider,embed,widgets
+from ipywidgets import interact,fixed,IntSlider,embed,widgets, interactive
 import ipywidgets
 import streamlit.components.v1 as components
-widg = interact(vismol,bxi=ipywidgets.IntSlider(min=-100,max=100, step=1) ,byi=ipywidgets.IntSlider(min=-100,max=100, step=1),
+cc = interactive(vismol,bxi=ipywidgets.IntSlider(min=-100,max=100, step=1) ,byi=ipywidgets.IntSlider(min=-100,max=100, step=1),
         bzi=ipywidgets.IntSlider(min=-100,max=100, step=1),bxf=ipywidgets.IntSlider(min=0,max=30, step=1),
         byf=ipywidgets.IntSlider(min=0,max=30, step=1),
         bzf=ipywidgets.IntSlider(min=0,max=30, step=1))
-st.write(widg)
-# widg = widgets.Box([cc], layout=widgets.Layout(height="700px",width="700px"))
+
+st.write(type(cc))
+widg = widgets.Box([cc], layout=widgets.Layout(height="700px",width="700px"))
 sc = embed.embed_snippet(widg)
 html = embed.html_template.format(title="", snippet=sc)
-components.html(html)
+components.html(html,width=1200, height=1200)
